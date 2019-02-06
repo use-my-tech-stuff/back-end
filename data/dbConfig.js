1501614b5402e3da1dabcdb9dfd491e4fd2cab5b
Luisan327 <luisangelsc@gmail.com>
2019-02-05 21:44:53 -0600

    fixed syntax mistake

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,6 +4,6 @@
 // module.exports = knex(knexConfig.development);
 
 const dbEngine = process.env.DB || 'production';
-const config = require('../knexfile.js')[dbEngine;]
+const config = require('../knexfile.js')[dbEngine];
 
 module.exports = require('knex')(config);
